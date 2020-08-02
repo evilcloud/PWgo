2f5af70cdc08ee756bc04f79eabc3714c99c394f
evilcloud <43460664+evilcloud@users.noreply.github.com>
2020-08-02 21:17:36 +0800

    black-cleaned

--- utils/dclean.py
+++ utils/dclean.py
@@ -5,30 +5,36 @@ import time
 
 
 def load_settings():
-    f = open('dclean.json')
+    f = open("dclean.json")
     settings = json.load(f)
     f.close()
     return settings
 
+
 def is_dir(dir: str) -> bool:
     return os.path.isdir(dir)
 
+
 def is_file(directory, filename: str) -> bool:
     return os.path.isfile(os.path.join(directory, filename))
+
 
 def confirm_or_exit(message: str = "") -> bool:
     print(message)
     return input("[ENTER] to continue, [n] to quit: ") == ""
 
+
 def get_all_txt(directory) -> list:
     return [txtfile for txtfile in os.listdir(directory) if txtfile.endswith(".txt")]
+
 
 def vertical_print(files: list):
     for name in files:
         print(f"\t{name}")
 
+
 def find_directory(args: list):
-    pfiles=[]
+    pfiles = []
     directory = ""
     for item in args:
         if item.startswith("@"):
@@ -36,6 +42,7 @@ def find_directory(args: list):
             continue
         pfiles.append(item)
     return pfiles, directory
+
 
 def clean_content(content):
     new_content = ""
@@ -51,12 +58,11 @@ def get_from_file(filepath) -> list:
     return content
 
 
-
 def main():
     tick = time.time()
     settings = load_settings()
-    directory = settings['directory']
-    pfiles = settings['files']
+    directory = settings["directory"]
+    pfiles = settings["files"]
 
     args, pdirectory = find_directory(sys.argv)
 
@@ -89,15 +95,13 @@ def main():
             os.rename(filepath, os.path.join(directory, "old_" + filename))
             print(f"Renamed {filename} to {'old_'+filename}")
         new_content = clean_content(content)
-            
+
         print(f"Writing the clean content into the file")
         with open(filepath, "w") as f:
             f.writelines(new_content)
         content = get_from_file(filepath)
-        print(f'Checked {filename} for {len(content)} lines')
+        print(f"Checked {filename} for {len(content)} lines")
     print(f"All done...\nexecution time {time.time() - tick} sec")
-
-
 
 
 if __name__ == "__main__":
